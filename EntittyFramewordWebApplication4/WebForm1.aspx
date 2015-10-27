b8b9dac4eaaf279855d3f96b044ffe423b4bfe97
sabbirziauddin <sabbir07bsc@gmail.com>
2015-10-27 12:22:50 +0600

    init commit

---
+++ EntittyFramewordWebApplication4/WebForm1.aspx
@@ -0,0 +1,109 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="EntittyFramewordWebApplication4.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <p>
+&nbsp;&nbsp;
+            &nbsp;
+            <asp:Button ID="Button6" runat="server" OnClick="Button6_Click" Text="Show all by gridvies" />
+        </p>
+        <p>
+            <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:Label ID="Label2" runat="server" Text="search by marks"></asp:Label>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:TextBox ID="TextBox1" runat="server" OnTextChanged="TextBox1_TextChanged"></asp:TextBox>
+        &nbsp;&nbsp;
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="show by marks" />
+        </p>
+        <p>
+            <asp:Label ID="Label3" runat="server" Text="id"></asp:Label>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+&nbsp;&nbsp;
+        </p>
+        <p>
+            <asp:Label ID="Label4" runat="server" Text="name"></asp:Label>
+&nbsp;
+            <asp:TextBox ID="TextBox3" runat="server" OnTextChanged="TextBox3_TextChanged"></asp:TextBox>
+        </p>
+        <p>
+            <asp:Label ID="Label5" runat="server" Text="address"></asp:Label>
+&nbsp;&nbsp;&nbsp;
+            <asp:Label ID="Label8" runat="server" Text="dep no:"></asp:Label>
+&nbsp;<asp:TextBox ID="TextBox7" runat="server"></asp:TextBox>
+            <asp:TextBox ID="TextBox4" runat="server"></asp:TextBox>
+        </p>
+        <p>
+            <asp:Label ID="Label6" runat="server" Text="section"></asp:Label>
+&nbsp;<asp:TextBox ID="TextBox5" runat="server"></asp:TextBox>
+        </p>
+        <p>
+            <asp:Label ID="Label7" runat="server" Text="marks"></asp:Label>
+&nbsp;<asp:TextBox ID="TextBox6" runat="server"></asp:TextBox>
+        &nbsp;
+            <asp:Button ID="Button2" runat="server" OnClick="Button2_Click" Text="Save by fixed" Width="90px" />
+        </p>
+        <p>
+            <asp:Button ID="Button3" runat="server" OnClick="Button3_Click" Text="update" Width="56px" style="height: 26px" />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:Button ID="Button4" runat="server" OnClick="Button4_Click" Text="Delate" />
+&nbsp;&nbsp;
+            <asp:Button ID="Button5" runat="server" OnClick="Button5_Click" Text="find" />
+        &nbsp;
+            <asp:Button ID="Button7" runat="server" OnClick="Button7_Click" Text="Add Employee" />
+        </p>
+        <p>
+            &nbsp;</p>
+        <p>
+            &nbsp;</p>
+        <p>
+            &nbsp;</p>
+        <p>
+            <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False" Width="400px">
+                <Columns>
+                    <asp:BoundField DataField="id" HeaderText="id" />
+                    <asp:BoundField DataField="name" HeaderText="name" />
+                    <asp:BoundField DataField="address" HeaderText="address" />
+                    <asp:BoundField DataField="section" HeaderText="section" />
+                    <asp:BoundField DataField="marks" HeaderText="marks" />
+                </Columns>
+            </asp:GridView>
+        </p>
+        <p>
+            &nbsp;</p>
+        <p>
+            &nbsp;</p>
+        <p>
+            &nbsp;</p>
+        <p>
+            &nbsp;</p>
+        <p>
+            &nbsp;</p>
+        <p>
+            &nbsp;</p>
+        <p>
+            &nbsp;</p>
+        <p>
+            &nbsp;</p>
+        <p>
+            &nbsp;</p>
+        <p>
+            &nbsp;</p>
+        <p>
+            &nbsp;</p>
+        <p>
+            &nbsp;</p>
+        <p>
+            &nbsp;</p>
+        <p>
+            &nbsp;</p>
+    </form>
+</body>
+</html>
---
+++ EntittyFramewordWebApplication4/WebForm1.aspx.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace EntittyFramewordWebApplication4
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            int m =int.Parse(TextBox1.Text);
+            EMSEntities dbcontext=new EMSEntities();
+            var stud = from dd in dbcontext.StudentDetails 
+            where
+            dd.marks > m && dd.section == "A"
+                       select dd;
+
+            //string s = "Output";
+            DataTable dt=new DataTable();
+            dt.Columns.Add("id");
+            dt.Columns.Add("name");
+            dt.Columns.Add("address");
+            dt.Columns.Add("section");
+            dt.Columns.Add("marks");
+            foreach (var student  in stud)
+            {
+                DataRow r = dt.NewRow();
+                r[0] = student.id;
+                r[1] = student.name;
+                r[2] = student.address;
+                r[3] = student.section;
+                r[4] = student.marks;
+                //r[0] = "id";
+                //r[1] = "name";
+                //r[2] = "address";
+                //r[3] = "section";
+                //r[4] = 4;
+
+
+                dt.Rows.Add(r);
+                //s = s + "ID:" + student.id;
+                //s = s + "name:" + student.name;
+                //s = s + "Section::" + student.section;
+
+            }
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+            //Label1.Text = s;
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            EMSEntities Context=new EMSEntities();
+
+           
+            EntittyFramewordWebApplication4.StudentDetail stud1=new StudentDetail();
+            stud1.id = "67";
+            stud1.name = "ziauddin";
+            stud1.address = "chittagong";
+            stud1.marks = 88;
+            stud1.section = "C";
+            Context.StudentDetails.Add(stud1);
+            Context.SaveChanges();
+            Label1.Text = "added";
+
+
+        }
+
+        protected void TextBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button5_Click(object sender, EventArgs e)
+        {
+            string id = TextBox2.Text;
+            EMSEntities dbcontext = new EMSEntities();
+            var stud = from dd in dbcontext.StudentDetails
+                       where dd.id==id select dd;
+            bool flag = false;
+            foreach (var student in stud)
+            {
+                TextBox3.Text = student.name;
+                TextBox4.Text = student.address;
+                TextBox5.Text = student.section;
+                TextBox6.Text = student.marks.ToString();
+                flag = true;
+
+            }
+            if (flag == false)
+                Label1.Text = "not found";
+            else
+            {
+                Label1.Text = "found";
+            }
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            EMSEntities dbcontext = new EMSEntities();
+            EntittyFramewordWebApplication4.StudentDetail stud1 = new StudentDetail();
+            try
+
+            {
+                string idd = TextBox2.Text;
+            
+            stud1 = dbcontext.StudentDetails.FirstOrDefault(xx => xx.id == idd);
+            stud1.marks = int.Parse(TextBox6.Text);
+            stud1.section = TextBox5.Text;
+            dbcontext.SaveChanges();
+            Label1.Text = "updated successfull";
+        }
+            catch(Exception e1)
+            {
+                Label1.Text = "not found";
+            }
+            
+
+    }
+
+        protected void Button4_Click(object sender, EventArgs e)
+        {
+            EMSEntities dbcontext = new EMSEntities();
+            EntittyFramewordWebApplication4.StudentDetail stud1 = new StudentDetail();
+            try
+            {
+                string idd = TextBox2.Text;
+
+                stud1 = dbcontext.StudentDetails.FirstOrDefault(xx => xx.id == idd);
+                dbcontext.StudentDetails.Remove(stud1);
+                dbcontext.SaveChanges();
+                Label1.Text = "delated successfull";
+            }
+            catch (Exception e1)
+            {
+                Label1.Text = "not found";
+            }
+
+        }
+
+        protected void Button6_Click(object sender, EventArgs e)
+        {
+            //int m = int.Parse(TextBox1.Text);
+            EMSEntities dbcontext = new EMSEntities();
+            var stud = from dd in dbcontext.StudentDetails
+                    
+                      select dd;
+
+            //string s = "Output";
+            DataTable dt = new DataTable();
+            dt.Columns.Add("id");
+            dt.Columns.Add("name");
+            dt.Columns.Add("address");
+            dt.Columns.Add("section");
+            dt.Columns.Add("marks");
+            foreach (var student in stud)
+            {
+                DataRow r = dt.NewRow();
+                r[0] = student.id;
+                r[1] = student.name;
+                r[2] = student.address;
+                r[3] = student.section;
+                r[4] = student.marks;
+               
+                //r[0] = "id";
+                //r[1] = "name";
+                //r[2] = "address";
+                //r[3] = "section";
+                //r[4] = 4;
+
+
+                dt.Rows.Add(r);
+                //s = s + "ID:" + student.id;
+                //s = s + "name:" + student.name;
+                //s = s + "Section::" + student.section;
+
+            }
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+        }
+
+        protected void Button7_Click(object sender, EventArgs e)
+        {
+            //EMSEntities dbcontext = new EMSEntities();
+            //emp e1 = new emp();
+            //e1.empId = int.Parse(TextBox2.Text);
+            //e1.name = TextBox4.Text;
+            //e1.deptno = int.Parse(TextBox5.Text);
+            //e1.job = TextBox5.Text;
+            //e1.salary = int.Parse(TextBox6.Text);
+            //dbcontext.emps.Add(e1);
+            //dbcontext.SaveChanges();
+            //Label1.Text = "added from emp table";
+            EMSEntities dbcontext = new EMSEntities();
+            emp e1 = new emp();
+            e1.empId = 786;
+            e1.name = "pabel";
+            e1.deptno = 56;
+            e1.job = "hr";
+            e1.salary = 6700;
+            dbcontext.emps.Add(e1);
+            dbcontext.SaveChanges();
+            Label1.Text = "added from emp table";
+        }
+
+
+
+
+        protected void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        
+    }
+}
